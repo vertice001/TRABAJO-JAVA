298c622bd501d518e5de0440ad69c289c61916bd
esteban vidal <esteban.vidalmoya20@gmail.com>
2023-09-01 01:14:39 -0400

    agregue la clase EmpleadoIndefinido con pagar finiquito y dar vacaciones

--- EJERCICIO-2-JAVA/src/EmpleadoIndefinido.java
+++ EJERCICIO-2-JAVA/src/EmpleadoIndefinido.java
@@ -1,3 +1,29 @@
-public class EmpleadoIndefinido {
-    
+class EmpleadoIndefinido extends Empleado {
+    public EmpleadoIndefinido(String nombre, String apellido, int salario, int tiempoContrato) {
+        super(nombre, apellido, salario, tiempoContrato);
+
+        if (tiempoContrato > 6) {
+            this.tieneVacaciones = true;
+        } else {
+            this.tieneVacaciones = false;
+        }
+    }
+
+    @Override
+    public int getPagarFiniquito() {
+        if (tiempoContrato >= 12) {
+            return super.getPagarFiniquito() + 50000;
+        }
+        return super.getPagarFiniquito();
+    }
+
+    @Override
+    public int darVacaciones() {
+        if (tieneVacaciones) {
+            return super.darVacaciones();
+        } else {
+            throw new IllegalArgumentException("no le corresponde vacaciones ");
+
+        }
+    }
 }

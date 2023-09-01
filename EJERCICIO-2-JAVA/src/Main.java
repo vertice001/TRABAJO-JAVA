234694ceaf0de5b5a887f5dbc5e0e3aae7d6dcaf
esteban vidal <esteban.vidalmoya20@gmail.com>
2023-09-01 02:49:02 -0400

    guardando main en la rama origin

--- EJERCICIO-2-JAVA/src/Main.java
+++ EJERCICIO-2-JAVA/src/Main.java
@@ -1,3 +1,20 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        try {
+            EmpleadoIndefinido empleadoIndefinido = new EmpleadoIndefinido("esteban", "vidal ", 500000, 14);
+            empleadoIndefinido.informacion();
+            System.out.println("Finiquito a pagar: " + empleadoIndefinido.getPagarFiniquito());
+            System.out.println("Días de vacaciones: " + empleadoIndefinido.darVacaciones());
+
+            System.out.println("***************************************************************");
+
+            EmpleadoTemporal empleadoTemporal = new EmpleadoTemporal("victor", "moya", 300000, 3);
+            empleadoTemporal.informacion();
+            empleadoTemporal.extenderContrato(3);
+            System.out.println("Contrato renovado: " + empleadoTemporal.isRenovado());
+            System.out.println("Días de vacaciones: " + empleadoTemporal.darVacaciones());
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }

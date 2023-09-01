ef5e7de4a449f5be5fa17232cbf4d969fd23aa2a
esteban vidal <esteban.vidalmoya20@gmail.com>
2023-09-01 01:59:11 -0400

    definimos el atributo isRENOVARDO(BOOLEAN) Y EL CONTRUCTOR EmpleadoTemporal  y heredamos de la clase empleado

--- EJERCICIO-2-JAVA/src/EmpleadoTemporal.java
+++ EJERCICIO-2-JAVA/src/EmpleadoTemporal.java
@@ -1,3 +1,30 @@
-public class EmpleadoTemporal {
-    
+class EmpleadoTemporal extends Empleado {
+    private boolean isRenovado;
+
+    public EmpleadoTemporal(String nombre, String apellido, int salario, int tiempoContrato) {
+        super(nombre, apellido, salario, tiempoContrato);
+        this.isRenovado = false;
+        this.tieneVacaciones = false;
+    }
+
+    public boolean isRenovado() {
+        return isRenovado;
+    }
+
+    public void extenderContrato(int meses) {
+        this.isRenovado = true;
+        tiempoContrato += meses;
+        if (tiempoContrato > 6) {
+            this.tieneVacaciones = true;
+        }
+    }
+
+    @Override
+    public int darVacaciones() {
+        if (tieneVacaciones) {
+            return super.darVacaciones();
+        } else {
+            throw new IllegalArgumentException("no le corresponde vacaciones ");
+        }
+    }
 }
